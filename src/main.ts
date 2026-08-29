6d1bdc8b787319ace478a9a6b40ba2998f91af01
Pedro Sobral <pedrolusobral85@gmail.com>
2026-08-29 14:20:43 -0300

    feat: create catalog class

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,34 @@
-function main(): void{
+import { getPokemon } from "./service/pokemonAPIService.js";
+import { Catalog } from "./model/catalog.js";
+
+async function main(){
     console.log(`Hello World`);
+    
+    const catalog = new Catalog;
+    
+    const pikachu = await getPokemon(25);
+    
+    if(pikachu !== null){
+        catalog.addPokemon(pikachu);
+    }
+
+    const pikachuAgain = await getPokemon(25);
+    
+    if(pikachuAgain !== null){
+        catalog.addPokemon(pikachuAgain);
+    }
+
+    const charizard = await getPokemon(`charizard`);
+    
+    if(charizard !== null){
+        catalog.addPokemon(charizard);
+    }
+
+    catalog.listPokemons();
+
+    catalog.removePokemon(25);
+
+    catalog.listPokemons()
 }
 
 main();
---
+++ src/model/catalog.ts
@@ -0,0 +1,61 @@
+import type { PokemonResume } from "./pokemonResume.js"
+
+export class Catalog {
+    private pokemons: PokemonResume[] = [];
+
+    addPokemon(pokemon: PokemonResume){
+
+        const isExisting:boolean = this.pokemons.every((p) => p.id !== pokemon.id);
+        
+        if(!isExisting){
+            console.log(`Error: ${pokemon.name} is already in the catalog.`);
+            return null;
+        }
+
+        this.pokemons.push(pokemon);
+
+        console.log(`Done: ${pokemon.name} has been added to the catalog.`);
+    }
+
+    listPokemons(){
+        if (this.pokemons.length == 0 ){
+            console.log(`NOTICE Empty catalog.`);
+            return null;
+        }
+
+        console.log(``);
+        console.log(`Catalog:`)
+        console.log(``);
+        this.pokemons.forEach((pokemon) => {
+            console.log(`-------------------------`);
+            console.log(`| #${pokemon.id} Name: ${pokemon.name} |`);
+            console.log(`-------------------------`);
+            console.log(``);
+            console.log(`Stats:`);
+            console.log(``);
+            pokemon.stats.forEach((stat) => {
+                console.log(`Name: ${stat.name}`);
+                console.log(`Base Stat: ${stat.base_stat}`);
+                console.log(``);
+            });
+            console.log(`Types:`);
+            console.log(``);
+            pokemon.types.forEach((t) => {
+                console.log(t);
+            });
+            console.log(``);
+        });
+    }
+
+    removePokemon(id: number){
+        if (this.pokemons.length == 0 ){
+            console.log(`NOTICE Empty catalog.`);
+            return null;
+        }
+
+        const newCatalog: PokemonResume[] = this.pokemons.filter((pokemon) => pokemon.id !== id);
+        this.pokemons = newCatalog;
+
+        console.log(`Removed pokemon with id ${id}`);
+    }
+}
